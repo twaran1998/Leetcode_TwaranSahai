c68d22fedd998db1de1659351e0717cebb64fb4a
Twaran Sahai <121211711+twaran1998@users.noreply.github.com>
2024-12-18 21:31:09 -0500

    Time: 1094 ms (5.11%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+set N=N-1;
+RETURN (
+ifnull((select distinct Salary from Employee
+order by Salary desc
+limit 1 offset N),null)
+);
+END
